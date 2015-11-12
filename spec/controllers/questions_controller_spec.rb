1651a9aa58a053976c24c1475e5b6bdc0f8301cf
Ivan Lemeshev <vanlmshv@gmail.com>
2015-11-13 00:36:17 +0600

    add show action to questions controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,15 +4,28 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
     let(:questions) { create_list(:question, 2) }
 
-    before do
-      get :index
-    end
+    before { get :index }
+
     it 'populates an array of all questions' do
       expect(assigns(:questions)).to match_array(questions)
     end
 
     it 'renders index view' do
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+
+    before { get :show, id: question }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
     end
   end
 end
