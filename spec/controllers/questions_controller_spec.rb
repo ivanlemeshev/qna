783404ac01c1b95421859d940608ce7b2d4743ae
Ivan Lemeshev <vanlmshv@gmail.com>
2015-11-13 00:39:53 +0600

    add new action to questions controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -28,4 +28,16 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :show
     end
   end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'assigns a new Question to @question' do
+      expect(assigns(:question)).to be_a_new Question
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
 end
