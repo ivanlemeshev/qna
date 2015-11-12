4e67ce726ba13fe4138b6911df88a9186a316546
Ivan Lemeshev <vanlmshv@gmail.com>
2015-11-13 00:48:30 +0600

    refactoring of questions controller tests

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
   describe 'GET #index' do
     let(:questions) { create_list(:question, 2) }
 
@@ -16,8 +18,6 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #show' do
-    let(:question) { create(:question) }
-
     before { get :show, id: question }
 
     it 'assigns the requested question to @question' do
@@ -42,8 +42,6 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #edit' do
-    let(:question) { create(:question) }
-
     before { get :edit, id: question }
 
     it 'assigns the requested question to @question' do
