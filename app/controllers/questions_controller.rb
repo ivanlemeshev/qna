4c98457fed17ac91fe80239f4a9be7c5169137ae
Ivan Lemeshev <vanlmshv@gmail.com>
2015-11-13 00:45:47 +0600

    add edit action to questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,23 @@
 class QuestionsController < ApplicationController
+  before_action :load_question, only: [:show, :edit]
+
   def index
     @questions = Question.all
   end
 
   def show
-    @question = Question.find(params[:id])
   end
 
   def new
     @question = Question.new
+  end
+
+  def edit
+  end
+
+  private
+
+  def load_question
+    @question = Question.find(params[:id])
   end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -40,4 +40,18 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :new
     end
   end
+
+  describe 'GET #edit' do
+    let(:question) { create(:question) }
+
+    before { get :edit, id: question }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders edit view' do
+      expect(response).to render_template :edit
+    end
+  end
 end
