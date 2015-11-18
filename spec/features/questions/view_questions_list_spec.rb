f7e22a9dbb9da2f203faa05a8ead58c4faf6efe0
Ivan Lemeshev <vanlmshv@gmail.com>
2015-11-19 01:17:06 +0600

    add view list of a questions

--- spec/features/questions/view_questions_list_spec.rb
+++ spec/features/questions/view_questions_list_spec.rb
@@ -5,5 +5,10 @@ feature 'View a list of questions', %q(
   As an user
   I want to have ability to view a list of questions
 ) do
-  scenario 'User view a list of questions'
+  scenario 'User view a list of questions' do
+    @questions = create_list(:question, 2)
+    visit questions_path
+
+    expect(page).to have_content 'MyString', count: 2
+  end
 end
