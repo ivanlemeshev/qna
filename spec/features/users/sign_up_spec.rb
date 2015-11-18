d12a5bc1b1fde9dd8c78eeb2236d44614a767f1b
Ivan Lemeshev <vanlmshv@gmail.com>
2015-11-19 00:54:56 +0600

    add user sign up tests

--- spec/features/users/sign_up_spec.rb
+++ spec/features/users/sign_up_spec.rb
@@ -5,9 +5,72 @@ feature 'User sign up', %q(
   As an authenticated user
   I want to have ability to sign up
 ) do
-  scenario 'Authenticated user try to sign up'
+  given(:user) { create(:user) }
 
-  scenario 'Unauthenticated user try to sign up'
+  scenario 'New user try to sign up with valid email and password' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'new@test.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
 
-  scenario 'Registered user try to confirm registration'
+    expect(page).to have_content 'A message with a confirmation link has been sent to your email address.
+                                  Please follow the link to activate your account.'
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'New user try to sign up with already taken email' do
+    visit new_user_registration_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Email has already been taken'
+    expect(current_path).to eq user_registration_path
+  end
+
+  scenario 'New user try to sign up with blank email' do
+    visit new_user_registration_path
+    fill_in 'Email', with: nil
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
+
+    expect(page).to have_content "Email can't be blank"
+    expect(current_path).to eq user_registration_path
+  end
+
+  scenario 'New user try to sign up with invalid email' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'wrong_email'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Email is invalid'
+    expect(current_path).to eq user_registration_path
+  end
+
+  scenario 'New user try to sign up with blank password' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'new@test.com'
+    fill_in 'Password', with: nil
+    fill_in 'Password confirmation', with: nil
+    click_on 'Sign up'
+
+    expect(page).to have_content "Password can't be blank"
+    expect(current_path).to eq user_registration_path
+  end
+
+  scenario 'New user try to sign up with invalid password confirmation' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'new@test.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: nil
+    click_on 'Sign up'
+
+    expect(page).to have_content "Password confirmation doesn't match Password"
+    expect(current_path).to eq user_registration_path
+  end
 end
