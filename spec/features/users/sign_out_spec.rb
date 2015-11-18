d8ebdcfadd3f399feeeb7aebeb1d357754ab65a2
Ivan Lemeshev <vanlmshv@gmail.com>
2015-11-19 00:13:49 +0600

    user sign out

--- spec/features/users/sign_out_spec.rb
+++ spec/features/users/sign_out_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 feature 'User sign out', %q(
-  In order to be able to exit from system
+  In order to be able to exit from the system
   As an authenticated user
   I want to have ability to sign out
 ) do
-  scenario 'Authenticated user try to sign out'
+  scenario 'Authenticated user try to sign out' do
+    sign_in(create(:user))
+    click_on 'Sign out'
 
-  scenario 'Unauthenticated user try to sign out'
+    expect(page).to have_content 'Signed out successfully.'
+    expect(current_path).to eq root_path
+  end
+
+
+  scenario 'Unauthenticated user try to sign out' do
+    visit root_path
+
+    expect(page).to have_content 'Sign in'
+    expect(page).to have_no_content 'Signed out'
+  end
 end
