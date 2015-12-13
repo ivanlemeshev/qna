3378fb609ec88628645e16b0c499a36776da7d43
Ivan Lemeshev <vanlmshv@gmail.com>
2015-12-13 23:49:22 +0600

    add question view

--- spec/features/questions/view_question_with_answers_spec.rb
+++ spec/features/questions/view_question_with_answers_spec.rb
@@ -5,7 +5,18 @@ feature 'View question with a list of answers', %q(
   As an user
   I want to have ability to view questions with a list of answers
 ) do
-  given(:user) { create(:user) }
+  given(:question_author) { create(:user) }
+  given(:answer_author1)  { create(:user) }
+  given(:answer_author2)  { create(:user) }
+  given(:question)        { create(:question, user: question_author) }
+  given(:answer1)         { create(:answer, question: question, user: answer_author1) }
+  given(:answer2)         { create(:answer, question: question, user: answer_author2) }
 
-  scenario 'View question with a list of answers'
+  scenario 'View question with a list of answers' do
+    visit question_path(question)
+
+    expect(page).to have_content question.body
+    expect(page).to have_content answer1.body
+    expect(page).to have_content answer2.body
+  end
 end
